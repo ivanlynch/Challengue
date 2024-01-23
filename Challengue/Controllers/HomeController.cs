3beb30be7d57df8f7496e9d8e1bc38a20bf91b2d
Ivan Lynch <ivan.lynch@outlook.com.ar>
2024-01-22 23:41:52 -0300

    hibernate config in progress

--- Challengue/Controllers/HomeController.cs
+++ Challengue/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Challengue.Controllers
         {
             return View();
         }
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
